78ce7cbbc77807b33e48f56d193daee7f8d18f25
Kush Desai <kush.desai@yahoo.co.nz>
2023-05-15 13:45:18 +1200

    created empty classes for Shop, Team, and Stadium GUI

---
+++ src/main/StadiumScreen.java
@@ -0,0 +1,52 @@
+package main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+
+public class StadiumScreen {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StadiumScreen window = new StadiumScreen();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public StadiumScreen() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("Shop");
+		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		
+		JButton btnNewButton = new JButton("Shop");
+		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
+	}
+
+}
