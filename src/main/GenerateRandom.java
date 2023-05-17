7a3486abf4418967808ff58e16c5a21b9df7b175
yunu <yunu121@gmail.com>
2023-05-17 13:37:09 +1200

    Added GenerateRandom class

---
+++ src/main/GenerateRandom.java
@@ -0,0 +1,13 @@
+package main;
+import java.util.*;
+public class GenerateRandom {
+	private Random random;
+	public GenerateRandom() {
+		random = new Random();
+	}
+	
+	public ArrayList<Athlete> generateWeeklyAthletes() {
+		
+		
+	}
+}
